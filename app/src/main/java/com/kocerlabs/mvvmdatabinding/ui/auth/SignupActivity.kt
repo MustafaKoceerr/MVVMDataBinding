43e96966440f9b522d29ea302d419da0f1c60a18
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-12-05 12:08:26 +0300

    Video_1 Project setup

--- app/src/main/java/com/kocerlabs/mvvmdatabinding/ui/auth/SignupActivity.kt
+++ app/src/main/java/com/kocerlabs/mvvmdatabinding/ui/auth/SignupActivity.kt
@@ -1,17 +1,18 @@
-package com.kocerlabs.mvvmdatabinding
+package com.kocerlabs.mvvmdatabinding.ui.auth
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.kocerlabs.mvvmdatabinding.R
 
-class MainActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_signup)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root_signup)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
