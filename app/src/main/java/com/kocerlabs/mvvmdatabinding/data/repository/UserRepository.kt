12c411aaa8d7d290f0a48da2d5a0306f88ff2c7a
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-12-05 17:15:44 +0300

    repository added

---
+++ app/src/main/java/com/kocerlabs/mvvmdatabinding/data/repository/UserRepository.kt
@@ -0,0 +1,23 @@
+package com.kocerlabs.mvvmdatabinding.data.repository
+
+import com.kocerlabs.mvvmdatabinding.data.database.AppDatabase
+import com.kocerlabs.mvvmdatabinding.data.database.entity.User
+import com.kocerlabs.mvvmdatabinding.data.network.MyApi
+import com.kocerlabs.mvvmdatabinding.data.network.SafeApiRequest
+import com.kocerlabs.mvvmdatabinding.data.network.model.UserRequest
+import com.kocerlabs.mvvmdatabinding.data.network.model.UserResponse
+
+class UserRepository(
+    private val api: MyApi,
+    private val database: AppDatabase
+) : SafeApiRequest() {
+
+    // ### NETWORK OPERATIONS
+    suspend fun userLogin(userRequest: UserRequest): UserResponse =
+        apiRequest { api.userLogin(userRequest) }
+
+    // ### DB OPERATIONS
+    suspend fun saveUser(user: User) = database.getUserDao().upsert(user)
+
+    fun getUser() = database.getUserDao().getUser()
+}
